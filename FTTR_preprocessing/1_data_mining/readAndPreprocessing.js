2068f15f388028bdda45bff8171f26ab5b7b29a9
peter <peterhyun1234@gmail.com>
2020-08-27 13:37:43 +0900

    add some modules for reading data

--- FTTR_preprocessing/1_data_mining/readAndPreprocessing.js
+++ FTTR_preprocessing/1_data_mining/readAndPreprocessing.js
@@ -3,25 +3,30 @@
 const xlsx = require( "xlsx" );
 
 
-
 // @files 엑셀 파일을 가져온다.
 
 const excelFile = xlsx.readFile( "../data/201907_202007_data.xlsm" );
 
+const numbersOfSample = 50;
+let sheetIdx = 0;
+let degreeOfMachines = 4;
 
+while(sheetIdx < degreeOfMachines){
+    // @breif 엑셀 파일의 첫번째 시트의 정보를 추출
+    const sheetName = excelFile.SheetNames[sheetIdx]; 
+    const currSheet = excelFile.Sheets[sheetName]; 
+    
+    // @details 엑셀 파일의 첫번째 시트를 읽어온다.
+    console.log( "Reading sheet 1: " +  sheetName);
+    const jsonData = xlsx.utils.sheet_to_json( currSheet, { defval : ""} );
+    
+    let idx = 1;
+    while(jsonData[idx].ID){
+        //console.log( jsonData[idx].ID);
+        idx++;
+    }
 
-// @breif 엑셀 파일의 첫번째 시트의 정보를 추출
+    console.log(idx);
 
-const sheetName = excelFile.SheetNames[0];          // @details 첫번째 시트 정보 추출
-
-const firstSheet = excelFile.Sheets[sheetName];       // @details 시트의 제목 추출
-
-
-
-// @details 엑셀 파일의 첫번째 시트를 읽어온다.
-
-const jsonData = xlsx.utils.sheet_to_json( firstSheet, { defval : "" } );
-
-
-
-console.log( jsonData );
+    sheetIdx++; // next sheet
+}
