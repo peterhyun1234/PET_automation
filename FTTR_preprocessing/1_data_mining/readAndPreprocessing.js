0b51a9a7846b980d6b01e1576a27e8f1b77982d0
peter <peterhyun1234@gmail.com>
2020-08-28 15:33:28 +0900

    add parsing module

--- FTTR_preprocessing/1_data_mining/readAndPreprocessing.js
+++ FTTR_preprocessing/1_data_mining/readAndPreprocessing.js
@@ -4,8 +4,10 @@ const xlsx = require( "xlsx" );
 
 
 // @files 엑셀 파일을 가져온다.
+const studyTerm = "201912_202007";
+const excelFile = xlsx.readFile( "../data/" + studyTerm + "_data.xlsm" );
 
-const excelFile = xlsx.readFile( "../data/201907_202007_data.xlsm" );
+const dataOutput = xlsx.utils.book_new();
 
 const numbersOfSample = 50;
 let sheetIdx = 0;
@@ -38,7 +40,8 @@ Array.prototype.shuffle = function () {
 
 while(sheetIdx < degreeOfMachines){
 
-    let resultGroups = [[],[],[],[],[]];
+    let resultGroups = [];
+    let resultIDs = [[],[],[],[],[]];
     
     // @breif 엑셀 파일의 첫번째 시트의 정보를 추출
     const sheetName = excelFile.SheetNames[sheetIdx]; 
@@ -54,7 +57,6 @@ while(sheetIdx < degreeOfMachines){
 
     while(jsonData[idx].ID){
         // 연령별로 F/M 구분해서 배열에 저장
-        //console.log(jsonData[idx]);
         if(30 <= jsonData[idx].Age && jsonData[idx].Age < 40){
             if(jsonData[idx].Sex === 'F'){
                 femaleAgeGroups[0].push(jsonData[idx].ID);
@@ -88,52 +90,81 @@ while(sheetIdx < degreeOfMachines){
         }
         idx++;
     }
+    //console.log(jsonData.length);
     
     // 랜덤으로 남자 최대 50, 여자 50 넣기
     for(let i = 0; i < maleAgeGroups.length; i++){
+        let age = [30, 40, 50, 60, 70];
+        let femaleCnt;
+        let maleCnt;
         if(maleAgeGroups[i].length + femaleAgeGroups[i].length <= numbersOfSample*2){ // 전체 표본이 100개 이하인 경우
             while(maleAgeGroups[i].length){
-                resultGroups[i].push(maleAgeGroups[i].pop());
+                resultIDs[i].push(maleAgeGroups[i].pop());
             }
             while(femaleAgeGroups[i].length){
-                resultGroups[i].push(femaleAgeGroups[i].pop());
+                resultIDs[i].push(femaleAgeGroups[i].pop());
             }
         }else{
             if(maleAgeGroups[i].length <= numbersOfSample){ // 남자가 50명 이하인 경우
                 while(maleAgeGroups[i].length){
-                    resultGroups[i].push(maleAgeGroups[i].pop());
+                    resultIDs[i].push(maleAgeGroups[i].pop());
                 }
                 // 여자는 랜덤화해서 50명 추출 후 push
                 femaleAgeGroups[i].shuffle();
-                let femaleCnt = 0;
+                femaleCnt = 0;
                 while(femaleCnt < numbersOfSample){
-                    resultGroups[i].push(femaleAgeGroups[i].pop());
+                    resultIDs[i].push(femaleAgeGroups[i].pop());
                     femaleCnt++;
                 }
 
             }else{ // 남자, 여자 둘다 랜덤화해서 추출 후 push
                 maleAgeGroups[i].shuffle();
-                let maleCnt = 0;
+                maleCnt = 0;
                 while(maleCnt < numbersOfSample){
-                    resultGroups[i].push(maleAgeGroups[i].pop());
+                    resultIDs[i].push(maleAgeGroups[i].pop());
                     maleCnt++;
                 }
 
                 femaleAgeGroups[i].shuffle();
-                let femaleCnt = 0;
+                femaleCnt = 0;
                 while(femaleCnt < numbersOfSample){
-                    resultGroups[i].push(femaleAgeGroups[i].pop());
+                    resultIDs[i].push(femaleAgeGroups[i].pop());
                     femaleCnt++;
                 }
             }
         }
+        //console.log("group " + age[i] + ": [ Female count: " + femaleCnt + ", Male count: " + maleCnt + "]");
 
-        console.log(resultGroups[i].length);
+        for(let j = 0; j < resultIDs[i].length; j++){
+            let foundInfo = jsonData.find(element => element.ID == resultIDs[i][j]);
+            resultGroups.push(foundInfo);
+        }
+
     }
 
+    // resultGroups를 sheet별로 그대로 넣어주면 됨!
 
 
+    // const currInfos = xlsx.utils.json_to_sheet(resultGroups, {skipHeader: false });
+
+    // // @breif CELL 넓이 지정
+
+    // currInfos["!cols"] = [
+    //     { wpx: 80 }   // A열
+    //     , { wpx: 80 }   // B열
+    //     , { wpx: 80 }    // C열
+    //     , { wpx: 60 }    // D열
+    //     , { wpx: 60 }    // D열
+    // ]
+
+
+
+    //xlsx.utils.book_append_sheet(dataOutput, currInfos, sheetName);
+
+    
     sheetIdx++; // next sheet
 }
 
+// files 엑셀파일을 생성하고 저장한다.
+//xlsx.writeFile(dataOutput, studyTerm + "_mined_data.xlsx"); 
 
