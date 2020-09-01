92b8fe890b77975d3263741ce4ff7f710941c9d7
peter <peterhyun1234@gmail.com>
2020-09-01 12:06:30 +0900

    add merging modules

---
+++ FTTR_preprocessing/2_sharpness_measurement/mergingIDs.js
@@ -0,0 +1,31 @@
+const xlsx = require( "xlsx" );
+const fs = require('fs');
+
+const studyTerm = "201912_202007";
+const excelFile = xlsx.readFile( "../1_data_mining/result/" + studyTerm + "/" + studyTerm + "_mined_data.xlsx" );
+let sheetIdx = 0;
+let degreeOfMachines = 4;
+
+while(sheetIdx < degreeOfMachines){
+    
+    const sheetName = excelFile.SheetNames[sheetIdx]; 
+    const currSheet = excelFile.Sheets[sheetName]; 
+
+    console.log( "Reading sheet 1: " +  sheetName);
+    const jsonData = xlsx.utils.sheet_to_json( currSheet, { defval : ""} );
+    
+    let data_len = jsonData.length;
+    let outputString = "";
+    
+    for(let i = 0; i < data_len; i++){
+        if(i === data_len-1){
+            outputString += jsonData[i].ID;
+        }else{
+            outputString += jsonData[i].ID + ", ";
+        }
+    }
+    //console.log(outputString);
+    fs.writeFileSync("merged_IDs.txt", '\ufeff' + outputString, {encoding: 'utf8'});
+    
+    sheetIdx++; // next sheet
+}
