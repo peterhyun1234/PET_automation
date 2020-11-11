acaf885d90a5055908a4085121b953dd1c119472
peterhyun <peterhyun1234@gmail.com>
2020-11-11 14:43:36 +0900

    add condition for each IDs

--- FTTR_preprocessing/1_data_mining/readAndPreprocessing_woc.js
+++ FTTR_preprocessing/1_data_mining/readAndPreprocessing_woc.js
@@ -5,7 +5,7 @@ const xlsx = require( "xlsx" );
 
 // @files 엑셀 파일을 가져온다.
 const studyTerm = "201912_202007";
-const excelFile = xlsx.readFile( "../data/" + studyTerm + "_data.xlsm" );
+const excelFile = xlsx.readFile( "../data/" + studyTerm + "_data.xlsx" );
 
 const dataOutput = xlsx.utils.book_new();
 
@@ -47,103 +47,105 @@ while(sheetIdx < degreeOfMachines){
     const sheetName = excelFile.SheetNames[sheetIdx]; 
     const currSheet = excelFile.Sheets[sheetName]; 
     
+    console.log( "Reading sheet "+ sheetIdx +": " +  sheetName);
+    
     // @details 엑셀 파일의 첫번째 시트를 읽어온다.
-    console.log( "Reading sheet 1: " +  sheetName);
+    let idx = 1;
     const jsonData = xlsx.utils.sheet_to_json( currSheet, { defval : ""} );
     
-    let idx = 1;
     let femaleAgeGroups = [[],[],[],[],[]];
     let maleAgeGroups = [[],[],[],[],[]];
 
-    while(jsonData[idx].ID){
+    while(idx < jsonData.length){
         // 연령별로 F/M 구분해서 배열에 저장
-        if(30 <= jsonData[idx].Age && jsonData[idx].Age < 40){
-            if(jsonData[idx].Sex === 'F'){
-                femaleAgeGroups[0].push(jsonData[idx].ID);
-            }else{
-                maleAgeGroups[0].push(jsonData[idx].ID);
-            }
-        }else if(40 <= jsonData[idx].Age && jsonData[idx].Age < 50){
-            if(jsonData[idx].Sex === 'F'){
-                femaleAgeGroups[1].push(jsonData[idx].ID);
-            }else{
-                maleAgeGroups[1].push(jsonData[idx].ID);
-            }
-        }else if(50 <= jsonData[idx].Age && jsonData[idx].Age < 60){
-            if(jsonData[idx].Sex === 'F'){
-                femaleAgeGroups[2].push(jsonData[idx].ID);
-            }else{
-                maleAgeGroups[2].push(jsonData[idx].ID);
-            }
-        }else if(60 <= jsonData[idx].Age && jsonData[idx].Age < 70){
-            if(jsonData[idx].Sex === 'F'){
-                femaleAgeGroups[3].push(jsonData[idx].ID);
-            }else{
-                maleAgeGroups[3].push(jsonData[idx].ID);
-            }
-        }else if(70 <= jsonData[idx].Age && jsonData[idx].Age < 80){
-            if(jsonData[idx].Sex === 'F'){
-                femaleAgeGroups[4].push(jsonData[idx].ID);
-            }else{
-                maleAgeGroups[4].push(jsonData[idx].ID);
-            }
-        }
+        console.log(jsonData[idx].ID);
+        // if(30 <= jsonData[idx].Age && jsonData[idx].Age < 40){
+        //     if(jsonData[idx].Sex === 'F'){
+        //         femaleAgeGroups[0].push(jsonData[idx].ID);
+        //     }else{
+        //         maleAgeGroups[0].push(jsonData[idx].ID);
+        //     }
+        // }else if(40 <= jsonData[idx].Age && jsonData[idx].Age < 50){
+        //     if(jsonData[idx].Sex === 'F'){
+        //         femaleAgeGroups[1].push(jsonData[idx].ID);
+        //     }else{
+        //         maleAgeGroups[1].push(jsonData[idx].ID);
+        //     }
+        // }else if(50 <= jsonData[idx].Age && jsonData[idx].Age < 60){
+        //     if(jsonData[idx].Sex === 'F'){
+        //         femaleAgeGroups[2].push(jsonData[idx].ID);
+        //     }else{
+        //         maleAgeGroups[2].push(jsonData[idx].ID);
+        //     }
+        // }else if(60 <= jsonData[idx].Age && jsonData[idx].Age < 70){
+        //     if(jsonData[idx].Sex === 'F'){
+        //         femaleAgeGroups[3].push(jsonData[idx].ID);
+        //     }else{
+        //         maleAgeGroups[3].push(jsonData[idx].ID);
+        //     }
+        // }else if(70 <= jsonData[idx].Age && jsonData[idx].Age < 80){
+        //     if(jsonData[idx].Sex === 'F'){
+        //         femaleAgeGroups[4].push(jsonData[idx].ID);
+        //     }else{
+        //         maleAgeGroups[4].push(jsonData[idx].ID);
+        //     }
+        // }
         idx++;
     }
-    //console.log(jsonData.length);
+    // console.log(jsonData.length);
     
     // 랜덤으로 남자 최대 25, 여자 25 넣기
-    for(let i = 0; i < maleAgeGroups.length; i++){
-        let age = [30, 40, 50, 60, 70];
-        let femaleCnt = 0;
-        let maleCnt = 0;
+    // for(let i = 0; i < maleAgeGroups.length; i++){
+    //     let age = [30, 40, 50, 60, 70];
+    //     let femaleCnt = 0;
+    //     let maleCnt = 0;
 
-        if(maleAgeGroups[i].length + femaleAgeGroups[i].length <= numbersOfSample * 2){ // 전체 표본이 100개 이하인 경우
-            while(maleAgeGroups[i].length){
-                resultIDs[i].push(maleAgeGroups[i].pop());
-                femaleCnt++;
-            }
-            while(femaleAgeGroups[i].length){
-                resultIDs[i].push(femaleAgeGroups[i].pop());
-                maleCnt++;
-            }
-        }else{
+    //     if(maleAgeGroups[i].length + femaleAgeGroups[i].length <= numbersOfSample * 2){ // 전체 표본이 100개 이하인 경우
+    //         while(maleAgeGroups[i].length){
+    //             resultIDs[i].push(maleAgeGroups[i].pop());
+    //             femaleCnt++;
+    //         }
+    //         while(femaleAgeGroups[i].length){
+    //             resultIDs[i].push(femaleAgeGroups[i].pop());
+    //             maleCnt++;
+    //         }
+    //     }else{
             
-            if(maleAgeGroups[i].length <= numbersOfSample){ // 남자가 50명 이하인 경우
-                while(maleAgeGroups[i].length){
-                    resultIDs[i].push(maleAgeGroups[i].pop());
-                    maleCnt++;
-                }
-                // 여자는 랜덤화해서 50명 추출 후 push
-                femaleAgeGroups[i].shuffle();
-                while(femaleCnt < numbersOfSample){
-                    resultIDs[i].push(femaleAgeGroups[i].pop());
-                    femaleCnt++;
-                }
+    //         if(maleAgeGroups[i].length <= numbersOfSample){ // 남자가 50명 이하인 경우
+    //             while(maleAgeGroups[i].length){
+    //                 resultIDs[i].push(maleAgeGroups[i].pop());
+    //                 maleCnt++;
+    //             }
+    //             // 여자는 랜덤화해서 50명 추출 후 push
+    //             femaleAgeGroups[i].shuffle();
+    //             while(femaleCnt < numbersOfSample){
+    //                 resultIDs[i].push(femaleAgeGroups[i].pop());
+    //                 femaleCnt++;
+    //             }
 
-            }else{ // 남자, 여자 둘다 랜덤화해서 추출 후 push
-                maleAgeGroups[i].shuffle();
-                while(maleCnt < numbersOfSample){
-                    resultIDs[i].push(maleAgeGroups[i].pop());
-                    maleCnt++;
-                }
+    //         }else{ // 남자, 여자 둘다 랜덤화해서 추출 후 push
+    //             maleAgeGroups[i].shuffle();
+    //             while(maleCnt < numbersOfSample){
+    //                 resultIDs[i].push(maleAgeGroups[i].pop());
+    //                 maleCnt++;
+    //             }
 
-                femaleAgeGroups[i].shuffle();
-                femaleCnt = 0;
-                while(femaleCnt < numbersOfSample){
-                    resultIDs[i].push(femaleAgeGroups[i].pop());
-                    femaleCnt++;
-                }
-            }
-        }
-        console.log("group " + age[i] + ": [ Female: " + femaleCnt + ", Male: " + maleCnt + " ]");
+    //             femaleAgeGroups[i].shuffle();
+    //             femaleCnt = 0;
+    //             while(femaleCnt < numbersOfSample){
+    //                 resultIDs[i].push(femaleAgeGroups[i].pop());
+    //                 femaleCnt++;
+    //             }
+    //         }
+    //     }
+    //     console.log("group " + age[i] + ": [ Female: " + femaleCnt + ", Male: " + maleCnt + " ]");
 
-        for(let j = 0; j < resultIDs[i].length; j++){
-            let foundInfo = jsonData.find(element => element.ID == resultIDs[i][j]);
-            resultGroups.push(foundInfo);
-        }
+    //     for(let j = 0; j < resultIDs[i].length; j++){
+    //         let foundInfo = jsonData.find(element => element.ID == resultIDs[i][j]);
+    //         resultGroups.push(foundInfo);
+    //     }
 
-    }
+    // }
 
     // resultGroups를 sheet별로 그대로 넣어주면 됨!
 
